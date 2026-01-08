3d969d953d2802adaafb1ad22cdae97dac3145aa
dkr-sjr <rkdrjs4637@naver.com>
2026-01-08 09:23:02 +0900

    chore : eslint airbnb 규칙 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate, Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const categoryList = [
   'all',
@@ -13,7 +13,7 @@ const categoryList = [
 export default function Header() {
   return (
     <>
-      <div className='flex bg-white my-5 mx-25 gap-2'>
+      <div className="flex bg-white my-5 mx-25 gap-2">
         <h1 className="flex text-5xl text-center font-bold bg-blue-300 p-2 shadow-sm m-5">
           <span className="text-blue-300 bg-white px-3">
             WORLD
@@ -24,7 +24,7 @@ export default function Header() {
         </h1>
       </div>
       <div className="flex justify-center border-b border-t border-gray-300 px-10 py-2">
-        {categoryList.map((category)=>(
+        {categoryList.map((category) => (
           <Link key={category} to={category} className="text-2xl font-bold text-center mx-10">
             {category}
           </Link>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App';
 
 const queryClient = new QueryClient();
 
@@ -15,4 +15,4 @@ createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
-)
+);
